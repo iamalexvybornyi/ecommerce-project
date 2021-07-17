e5c10930ab37dae993bf0e4d42f2d58e3cd4f077
Alexander Vyborny <alexander.vyborny@sperasoft.com>
2021-07-17 14:11:24 +0300

    Initialize backend, add a few entities and repositories for them, add some starter files

---
+++ ecommerce-project-be/src/main/java/com/iamalexvybornyi/EcommerceProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.iamalexvybornyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProjectBeApplication.class, args);
+	}
+
+}
---
+++ ecommerce-project-be/src/test/java/com/iamalexvybornyi/EcommerceProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamalexvybornyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
