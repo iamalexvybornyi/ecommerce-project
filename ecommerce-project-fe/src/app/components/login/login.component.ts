895e9427ad034a56010114c720868b1dd60dae4c
Alexander Vyborny <iamalexvybornyi@gmail.com>
2021-10-21 20:54:56 +0300

    - Add the authentication/login functionality with OKTA to FE - Add dependency for API documentation generation to BE

---
+++ ecommerce-project-fe/src/app/components/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import myAppConfig from '../../config/my-app-config'
+import {OktaAuthStateService} from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import * as OktaSignIn from '@okta/okta-signin-widget'
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignIn: any;
+
+  constructor(
+    private oktaAuthService: OktaAuthStateService,
+    private oktaAuth: OktaAuth
+  ) {
+    this.oktaSignIn = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove();
+
+    this.oktaSignIn.renderEl({
+      el: '#okta-sign-in-widget' },
+      (response) => {
+      if (response.status === 'SUCCESS') {
+        this.oktaAuth.signInWithRedirect();
+      }
+      }
+    );
+  }
+
+}
